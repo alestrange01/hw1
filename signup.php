997789016c08346b229123db37b530e150364ad2
Alessandro Strano <75170697+alestrange01@users.noreply.github.com>
2023-05-29 20:49:27 +0200

    Last commit
    Final commit for homework1

--- signup.php
+++ signup.php
@@ -138,7 +138,7 @@
   <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>StrangeRoyaleSignUp</title>
+      <title>StrangeRoyale Sign Up</title>
       <link rel="stylesheet" href="signup.css"/>
       <link rel="stylesheet" href="footer.css">
       <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet">
